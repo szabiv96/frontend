53a4e9bef6b980268854c2664d52340f088209e3
szabiv96 <szabi.v96@gmail.com>
2023-08-21 17:44:40 +0200

    package.jsonban

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,30 +2,7 @@ import React, { useEffect } from 'react'
 
 export default function Footer() {
 
-    useEffect(() => {
-        const handleScroll2 = () => {
-            const scrollPosition = window.innerHeight + window.scrollY;
-            const pageHeight = document.documentElement.scrollHeight;
-            const scrollThreshold = 360;
-    
-            if(pageHeight - scrollPosition < scrollThreshold) {
-                console.log("hello world");
-            }
-        }
-
-        console.log(document.querySelector(".landingIMG"));
-
-        const newDivStyle = {
-            display: none,
-        };
-
-        window.addEventListener('scroll', handleScroll2);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll2);
-        }
-
-    }, []);
+   
 
     return (
         <div className='footer'>
