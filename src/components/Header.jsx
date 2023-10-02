462da93fb680216753f83565989a6fad8ff0c835
szabiv96 <szabi.v96@gmail.com>
2023-10-02 21:18:31 +0200

    10_02mobileOptim

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Header() {
+
+  const [show, setShow] = useState(false);
+  const [animation, setAnimation] = useState(false);
+
   return (
     <>
-      <div className='nav'>
+      <div className={animation ? "menuAnimation menuButton" : "menuButton"} onClick={() => {
+        console.log("hello there");
+        setShow(!show);
+        setAnimation(!animation);
+      }}>
+        <div className='menuPart01'></div>
+        <div className='menuPart02'></div>
+        <div className='menuPart03'></div>
+      </div>
+      <div className={show ? "showMenu nav" : "nav"}>
         <h4><a href="/">StarterPage</a></h4>
         <h4>:</h4>
         <h4><a href='/gallery'>CarefullyKept</a></h4>
