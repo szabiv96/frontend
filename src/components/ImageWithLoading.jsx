fddd161db93ee07930898e681b42611c0133da23
szabiv96 <szabi.v96@gmail.com>
2026-05-07 15:09:20 +0200

    took out the loading anim for the album's and covers cause it was broken

--- src/components/ImageWithLoading.jsx
+++ src/components/ImageWithLoading.jsx
@@ -1,30 +1,7 @@
-import { useEffect, useState } from 'react';
-
 function ImageWithLoading({ src, alt }) {
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        setIsLoading(Boolean(src));
-    }, [src]);
-
-    const handleImageLoad = () => {
-        setIsLoading(false);
-    };
-
-    const handleImageError = () => {
-        setIsLoading(false);
-    };
-
     return (
-        <div className={`image-container ${isLoading ? 'loading' : ''}`}>
-            {isLoading && <div className="loading-animation"></div>}
-            <img
-                src={src}
-                alt={alt}
-                onLoad={handleImageLoad}
-                onError={handleImageError}
-                className={isLoading ? 'hidden' : 'visible'}
-            />
+        <div className="image-container">
+            <img src={src} alt={alt} />
         </div>
     );
 }
